769a5d7755a2e0b8ecd70e1245bef52b53fcb76f
Fernanda da Costa Sousa <fernanda.csousa@b2wdigital.com>
2021-05-22 17:47:49 -0300

    Added linting configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import Counter from "features/counter/Counter";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const app: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -53,6 +53,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default app;
--- src/features/counter/counterAPI.ts
+++ src/features/counter/counterAPI.ts
@@ -1,6 +1,11 @@
-// A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+interface ResposeMock {
+  data: number;
+}
+
+const fetchCount = (amount = 1): Promise<ResposeMock> => {
   return new Promise<{ data: number }>((resolve) =>
     setTimeout(() => resolve({ data: amount }), 500)
   );
-}
+};
+
+export default fetchCount;
